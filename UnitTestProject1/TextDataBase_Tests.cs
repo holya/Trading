6d7d2f450d64c7d00eab4dafca7f594c5efef942
AlirezaOliya <alireza.olya@hotmail.com>
2018-10-18 22:09:57 +0330

    UnitTest created

---
+++ UnitTestProject1/TextDataBase_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trading.Databases.TextFileDataBase;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TextDataBase_Tests
+    {
+        [TestMethod]
+        public void SaveData__Observe_Valid_Data_Entry()
+        {
+            Assert.Fail();
+        }
+    }
+}
