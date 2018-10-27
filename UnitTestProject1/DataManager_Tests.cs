3288c263bce25b99c589978c980fc0dec94b84e9
AlirezaOliya <alireza.olya@hotmail.com>
2018-10-27 22:44:37 +0330

    added files tests and some psuedocode

---
+++ UnitTestProject1/DataManager_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trading.DataManager;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class DataManager_Tests
+    {
+        [TestMethod]
+        public void DataUpdate_Forex()
+        {
+            Assert.Fail();
+        }
+    }
+}
