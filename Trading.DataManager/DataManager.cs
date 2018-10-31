5ab90370a98ffc108ccd8bb69cc35521e943451d
AlirezaOliya <alireza.olya@hotmail.com>
2018-11-01 00:04:30 +0330

    added empty methods to DataManager

--- Trading.DataManager/DataManager.cs
+++ Trading.DataManager/DataManager.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 using Trading.Common;
 using Trading.DataProviders.Interfaces;
 using Trading.Databases.Interfaces;
+using Trading.Common.Instrument;
 
 namespace Trading.DataManager
 {
-    public class DataManager
+    public class DataManager : IDataBase, IDataProvider
     {
-        void UpdateFileData(IEnumerable<Bar> readData, Resolution resolution, DateTime latestDate)
+        public IEnumerable<Bar> ReadData(Instrument instrument, Resolution resolution)
         {
-            //should recieve read data from ReadData() and evaluate to what date append
-            // StreamWriter(path, TRUE) the boolean parameter appends if the file exits hence true
+            throw new NotImplementedException();
         }
+
+        public void WriteData(Instrument instrument, Resolution resolution, IEnumerable<Bar> barList)
+        {
+
+        }
+
+        public Task<IEnumerable<Bar>> GetHistoricalDataAsync(string symbol, Resolution resolution,
+            DateTime beginDateTime, DateTime endDateTime, bool subscribeToRealTime = false)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
