c0106b2a97e4028fef117e653fe38aeb8c30a07b
Hamid Olya <holya2000@hotmail.com>
2018-11-03 13:31:45 -0700

    .

--- Trading.DataManager/DataManager.cs
+++ Trading.DataManager/DataManager.cs
@@ -6,22 +6,11 @@ using System.Threading.Tasks;
 using Trading.Common;
 using Trading.DataProviders.Interfaces;
 using Trading.Databases.Interfaces;
-using Trading.Common.Instrument;
 
 namespace Trading.DataManager
 {
-    public class DataManager : IDataBase, IDataProvider
+    public class DataManager
     {
-        public IEnumerable<Bar> ReadData(Instrument instrument, Resolution resolution)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteData(Instrument instrument, Resolution resolution, IEnumerable<Bar> barList)
-        {
-
-        }
-
         public Task<IEnumerable<Bar>> GetHistoricalDataAsync(string symbol, Resolution resolution,
             DateTime beginDateTime, DateTime endDateTime, bool subscribeToRealTime = false)
         {
