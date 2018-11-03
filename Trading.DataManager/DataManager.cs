ddc0e8be450a2616a91d511d372795d30b04b661
AlirezaOliya <alireza.olya@hotmail.com>
2018-11-03 22:58:29 +0330

    added code to DataManager

--- Trading.DataManager/DataManager.cs
+++ Trading.DataManager/DataManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trading.Common;
+using Trading.Brokers.Fxcm;
 using Trading.DataProviders.Interfaces;
 using Trading.Databases.TextFileDataBase;
 using Trading.Common.Instrument;
@@ -12,6 +13,32 @@ namespace Trading.DataManager
 {
     public class DataManager
     {
+        TextDataBase db = new TextDataBase();
 
+        FxcmWrapper fxm = new FxcmWrapper();
+
+        public IEnumerable<Bar> RespondToUserDataRequest(Instrument instrument, Resolution resolution)
+        {
+            if (LocalDataReport(instrument, resolution) == true)
+            {
+                var localData = db.ReadData(instrument, resolution);
+                return localData;
+            }
+            else
+            {
+                var downloadedData = DataDownload(instrument, resolution);
+                return downloadedData;
+            }
+        }
+
+        public IEnumerable<Bar> DataDownload(Instrument instrument, Resolution resolution)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool LocalDataReport(Instrument instrument, Resolution resolution)
+        {
+            return false;
+        }
     }
 }
