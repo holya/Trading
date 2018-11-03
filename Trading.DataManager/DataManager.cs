af13c9c887c772107cc11c2a740f6fc3eb182185
AlirezaOliya <alireza.olya@hotmail.com>
2018-11-03 15:35:35 +0330

    migrated DataManager framework to .Net 4.6.1

--- Trading.DataManager/DataManager.cs
+++ Trading.DataManager/DataManager.cs
@@ -10,23 +10,8 @@ using Trading.Common.Instrument;
 
 namespace Trading.DataManager
 {
-    public class DataManager : IDataBase, IDataProvider
+    public class DataManager
     {
-        public IEnumerable<Bar> ReadData(Instrument instrument, Resolution resolution)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteData(Instrument instrument, Resolution resolution, IEnumerable<Bar> barList)
-        {
-
-        }
-
-        public Task<IEnumerable<Bar>> GetHistoricalDataAsync(string symbol, Resolution resolution,
-            DateTime beginDateTime, DateTime endDateTime, bool subscribeToRealTime = false)
-        {
-            throw new NotImplementedException();
-        }
 
     }
 }
