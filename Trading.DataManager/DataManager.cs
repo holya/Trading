e5c9dc4399962d5330e66285bdac8aeacb966270
AlirezaOliya <alireza.olya@hotmail.com>
2018-11-14 19:03:54 +0330

    moved symbols manager

--- Trading.DataManager/DataManager.cs
+++ Trading.DataManager/DataManager.cs
@@ -92,4 +92,36 @@ namespace Trading.DataManager
             }
         }
     }
+
+    public class SymbolsManager
+    {
+        private string docUri = "../../Symbols.xml";
+
+        public SymbolsManager()
+        {
+
+        }
+
+        public IEnumerable<string> GetForexPairsMajor()
+        {
+            return getForexPairs("major");
+        }
+        public IEnumerable<string> GetForexPairsMinor()
+        {
+            return getForexPairs("minor");
+        }
+
+        private IEnumerable<string> getForexPairs(string forexType)
+        {
+            var list = new List<string>();
+            var doc = XDocument.Load(docUri);
+
+            var elements = doc.Element("symbols").Element("forex").Element(forexType).Elements().Attributes("name");
+            foreach (var v in elements)
+                list.Add(v.Value);
+
+            return list;
+        }
+
+    }
 }
