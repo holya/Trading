8b8d721e5a9dcb24d47618aa89731b6806c2b207
AlirezaOliya <alireza.olya@hotmail.com>
2018-11-27 00:39:12 +0330

    InstrumentsManager

---
+++ Trading.DataManager/InstrumentsManager/ForexTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trading.DataManager
+{
+    public enum ForexTypes
+    {
+        major = 0,
+        minor = 1,
+        exotic = 2
+    }
+}
---
+++ Trading.DataManager/InstrumentsManager/InstrumentsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Trading.DataManager
+{
+    public class InstrumentsManager : IInstrumentsManager
+    {
+        private string docUri = "../../Symbols.xml";
+
+        public InstrumentsManager()
+        {
+
+        }
+
+        public IEnumerable<string> GetForexPairs(ForexTypes forexType)
+        {
+            var list = new List<string>();
+            var doc = XDocument.Load(docUri);
+
+            var elements = doc.Root.Element("forex").Element(forexType.ToString()).Elements().Attributes("name");
+            foreach (var v in elements)
+                list.Add(v.Value);
+
+            return list;
+        }
+    }
+}
