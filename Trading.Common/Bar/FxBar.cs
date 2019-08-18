b49646758b562052adcc66595500e23ba2b575f3
Alireza Oliya <alireza.olya@hotmail.com>
2019-08-19 00:32:39 +0430

    .

--- Trading.Common/Bar/FxBar.cs
+++ Trading.Common/Bar/FxBar.cs
@@ -16,7 +16,7 @@ namespace Trading.Common
 
         public FxBar(double open, double askOpen, double high, double askHigh, 
             double low, double askLow, double close, double askClose, double volume, 
-            DateTime dateTime, DateTime endDateTime) : base(open, high, low, close, volume, dateTime, endDateTime)
+            DateTime dateTime, DateTime endDateTime, FxBar previousBar = null) : base(open, high, low, close, volume, dateTime, endDateTime, previousBar)
         {
             this.AskOpen = askOpen;
             this.AskHigh = askHigh;
