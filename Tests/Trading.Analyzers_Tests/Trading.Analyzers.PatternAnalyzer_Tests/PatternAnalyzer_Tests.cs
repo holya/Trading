3ba0b95b6ab6fc2e9093039294c0abdccc080aa8
Hamid Olya <holya2000@hotmail.com>
2020-06-13 00:01:43 -0700

    PatternAnalyzer

--- Tests/Trading.Analyzers_Tests/Trading.Analyzers.PatternAnalyzer_Tests/PatternAnalyzer_Tests.cs
+++ Tests/Trading.Analyzers_Tests/Trading.Analyzers.PatternAnalyzer_Tests/PatternAnalyzer_Tests.cs
@@ -14,9 +14,13 @@ namespace Trading.Analyzers_Tests
     public class PatternAnalyzer_Tests
     {
         [TestMethod]
-        public void Sample()
+        public void Construct_First_Pattern()
         {
+            var bar = new Bar(10, 20, 5, 15, 0, DateTime.Now, DateTime.Now);
 
+            var p = new Pattern(bar);
+
+            Assert.AreEqual(PatternDirection.Up, p.Direction);
         }
     }
 }
