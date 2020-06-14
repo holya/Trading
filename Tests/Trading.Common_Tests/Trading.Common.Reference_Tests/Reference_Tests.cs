e9987ec0ff6744f0cfdcd9bacfd559909d97213e
Alireza Oliya <alireza.olya@hotmail.com>
2020-06-13 23:29:07 -0700

    Reference pseudocode + Tests

---
+++ Tests/Trading.Common_Tests/Trading.Common.Reference_Tests/Reference_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests;
+using Trading.Common;
+
+
+namespace Trading.Common.Reference_Tests
+{
+    [TestClass]
+    class Reference_Tests
+    {
+    }
+}
